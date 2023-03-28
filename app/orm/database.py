44431197330687029ffde167b7e772997dbd370e
Hadi Jamshidi <m.hadi.jamshidi@gmail.com>
2023-03-28 09:16:50 +0200

    shrink db pool

--- app/orm/database.py
+++ app/orm/database.py
@@ -13,7 +13,7 @@ SQLALCHEMY_DATABASE_URL = "postgresql://{}:{}@{}:{}/{}".format(
 )
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL,
-    pool_size=20, max_overflow=10
+    pool_size=2, max_overflow=1
 )
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
