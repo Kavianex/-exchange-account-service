d60a8273990e706dad37d01d7b669aa37c4b03f9
Hadi Jamshidi <m.hadi.jamshidi@gmail.com>
2023-04-04 10:50:04 +0200

    add dt log

--- app/engine.py
+++ app/engine.py
@@ -1,16 +1,18 @@
 from kafka.consumer import consume as kafka_concumer
 from internal import enums, match
+from datetime import datetime as dt
 import json
 import time
 
 
 def event_handler(event):
     t1 = time.time()
+    utc_now = dt.utcnow()
     event = json.loads(event)
     if event['topic'] == enums.EeventTopic.order_update.value:
         match.receive_order(event['event'])
     t2 = time.time()
-    print(f"done in {t2 - t1} s")
+    print(f"recv: {utc_now} done in {t2 - t1}s")
 
 
 if __name__ == "__main__":
